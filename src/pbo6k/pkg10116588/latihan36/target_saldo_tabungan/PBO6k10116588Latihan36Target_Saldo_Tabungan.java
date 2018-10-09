c821a751f27d344467bde62f01321a34cf8c41fb
Indra F <indrafebriasta@gmail.com>
2018-10-09 14:42:17 +0700

    Program Java Saldo Tabungan

---
+++ src/pbo6k/pkg10116588/latihan36/target_saldo_tabungan/PBO6k10116588Latihan36Target_Saldo_Tabungan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116588.latihan36.target_saldo_tabungan;
+
+/**
+ *
+ * @author Indra F
+ * Nama                     : Indra Febriastanto
+ * NIM                      : 10116588
+ * KELAS                    : PBO6K
+ * Deskripsi program        : Program ini berisi untuk menampilkan Target Saldo Tabungan
+ */
+public class PBO6k10116588Latihan36Target_Saldo_Tabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tabungan tabungan = new Tabungan();
+        tabungan.printTargetSaldo();
+    }
+    
+}
